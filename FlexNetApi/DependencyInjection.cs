2007ff8be8ac5cbfc80a6ee2c7c8a64a2b9448bf
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-10-03 19:30:46 +0200

    Setup: Create .NET solution with Clean Architecture project structure

---
+++ FlexNetApi/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using FlexNet.Application;
+using FlexNet.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlexNet.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddApplicationDI()
+                .AddInfrastructureDI(configuration);
+
+            return services;
+        }
+    }
+}
