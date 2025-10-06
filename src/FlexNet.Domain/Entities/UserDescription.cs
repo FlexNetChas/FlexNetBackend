39152841a30e0074b4475feee5919a3a235996de
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-06 13:03:14 +0200

    Updated structure added to gitignore

---
+++ src/FlexNet.Domain/Entities/UserDescription.cs
@@ -0,0 +1,12 @@
+namespace FlexNet.Domain.Entities;
+
+public class UserDescription
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public string Education { get; set; }
+    public string Purpose { get; set; }
+    public User? User { get; set; }
+}
