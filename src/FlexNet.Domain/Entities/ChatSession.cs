d1a3355b50925bf57c913a917f875d84070f5e4b
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-07 10:17:01 +0200

    refactor: make Summary and EndedTime nullable, remove defaults

--- src/FlexNet.Domain/Entities/ChatSession.cs
+++ src/FlexNet.Domain/Entities/ChatSession.cs
@@ -4,9 +4,9 @@ public class ChatSession
 {
     public int Id { get; set; }
     public int UserId { get; set; }
-    public string Summary { get; set; }
-    public DateTime StartedTime { get; set; } = DateTime.UtcNow;        
-    public DateTime EndedTime { get; set; }
+    public string? Summary { get; set; }
+    public DateTime StartedTime { get; set; }  
+    public DateTime? EndedTime { get; set; }
     public User? User { get; set; }
     public ICollection<ChatMessage> ChatMessages { get; set; } = new List<ChatMessage>();
 }
