814ed297521d16221ad6db379c82872c039d95d7
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-07 10:17:06 +0200

    refactor: remove default values from User entity properties

--- src/FlexNet.Domain/Entities/User.cs
+++ src/FlexNet.Domain/Entities/User.cs
@@ -6,10 +6,10 @@ public class User
     public string FirstName{ get; set; }
     public string LastName{ get; set; }
     public string Email{ get; set; }
-    public string Role { get; set; } = "User";
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string Role { get; set; } 
+    public DateTime CreatedAt { get; set; } 
     public string PasswordHash { get; set; }
-    public bool IsActive { get; set; } = true;
+    public bool IsActive { get; set; } 
 
     public ICollection<ChatSession> ChatSessions { get; set; } = new List<ChatSession>();
     public Avatar? Avatar { get; set; }
