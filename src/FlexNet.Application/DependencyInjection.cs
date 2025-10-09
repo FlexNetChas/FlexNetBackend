37c737b90a7e58bcdfc60666da62a1623cf96d8b
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-09 15:10:43 +0200

    removed redundant code

--- src/FlexNet.Application/DependencyInjection.cs
+++ src/FlexNet.Application/DependencyInjection.cs
@@ -3,18 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using FlexNet.Application.Interfaces.IServices;
 using FlexNet.Application.Services;
 
-namespace FlexNet.Application;
-
-public static class DependencyInjection
-{
-    public static IServiceCollection AddApplicationDI(this IServiceCollection services)
-    {
-        // Add Services
-        services.AddScoped<IUserService, UserService>();
-
-        return services;
-    }
-}
 namespace FlexNet.Application
 {
     public static class DependencyInjection
