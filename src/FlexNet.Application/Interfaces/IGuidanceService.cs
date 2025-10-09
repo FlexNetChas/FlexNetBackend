dcb51c2d0e32366d6e622a58a7071ef0f012b631
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-09 08:32:28 +0200

    feat: Add IGuidanceService interface for AI guidance
    - Define contract for guidance service with conversation and context support
    - Follow Dependency Inversion Principle
    - Prepare for conversation memory and student context features

---
+++ src/FlexNet.Application/Interfaces/IGuidanceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlexNet.Application.Models;
+
+namespace FlexNet.Application.Interfaces
+{
+    public interface IGuidanceService
+    {
+        Task<string> GetGuidanceAsync(string userMessage, IEnumerable<ConversationMessage> conversationHistory, StudentContext studentContext);
+    }
+}
