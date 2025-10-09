c9cd3a4a20f5bd9cf8496ac73e76970f910fabff
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-09 08:50:18 +0200

    feat: Added StudentContex and ConversationMessage to Models

---
+++ src/FlexNet.Application/Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexNet.Application.Models
+{
+    public record StudentContext(
+        int Age,
+        string? Gender,
+        string? Education,
+        string? Purpose);
+}
