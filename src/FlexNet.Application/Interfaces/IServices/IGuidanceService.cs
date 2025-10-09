66e4d954719e728beae3cd4afe899b9ed7cab256
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-09 15:44:46 +0200

    Add IGuidanceService interface to IServices namespace

---
+++ src/FlexNet.Application/Interfaces/IServices/IGuidanceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlexNet.Application.Models;
+
+namespace FlexNet.Application.Interfaces.IServices
+{
+    public interface IGuidanceService
+    {
+        Task<string> GetGuidanceAsync(string userMessage, IEnumerable<ConversationMessage> conversationHistory, StudentContext studentContext);
+    }
+}
