f9838659020bf58a438cbf7fe7254c465b31182c
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-10 14:34:19 +0200

    refactor: move StudentContext to Records folder

---
+++ src/FlexNet.Application/Models/Records/StudentContext.cs
@@ -0,0 +1,8 @@
+﻿namespace FlexNet.Application.Models.Records
+{
+    public record StudentContext(
+        int Age,
+        string? Gender,
+        string? Education,
+        string? Purpose);
+}
