a2158c232c227e352fa001335aacf8cc567780fb
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-10 14:34:37 +0200

    refactor: update IGuidanceService to return GuidanceResult

--- src/FlexNet.Application/Interfaces/IServices/IGuidanceService.cs
+++ src/FlexNet.Application/Interfaces/IServices/IGuidanceService.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FlexNet.Application.Models;
+using FlexNet.Application.Models.Records;
 
 namespace FlexNet.Application.Interfaces.IServices
 {
     public interface IGuidanceService
     {
-        Task<string> GetGuidanceAsync(string userMessage, IEnumerable<ConversationMessage> conversationHistory, StudentContext studentContext);
+        Task<GuidanceResult> GetGuidanceAsync(string userMessage, IEnumerable<ConversationMessage> conversationHistory,
+            StudentContext studentContext);
     }
 }
