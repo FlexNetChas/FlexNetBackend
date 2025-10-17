a1e9781775b9ac3052a18ebbe372fc0458202da9
JBrannelid <j.brannelid@icloud.com>
2025-10-17 17:46:15 +0200

    Add Auth register response DTO

---
+++ src/FlexNet.Application/DTOs/Auth/Response/RegisterResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using FlexNet.Application.DTOs.User;
+
+namespace FlexNet.Application.DTOs.Auth.Response
+{
+    public record RegisterResponseDto(
+        string AccessToken,
+        string RefreshToken,
+        UserDto User
+    );
+
+}
