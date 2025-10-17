05ffc3e0ebc74412d5f1622e5b60edbfab4da035
Rasmus Wenngren <rasmuswenngren@hotmail.com>
2025-10-17 11:05:20 +0200

    Add custom SecurityException for token reuse detection

--- src/FlexNet.Application/Exceptions/SecurityException.cs
+++ src/FlexNet.Application/Exceptions/SecurityException.cs
@@ -1,6 +1,15 @@
 namespace FlexNet.Application.Exceptions;
 
-public class SecurityException
+public class SecurityException : Exception
 {
+    public SecurityException(string message) : base(message)
+    {
+        
+    }
+
+    public SecurityException(string message, Exception innerException) : base(message, innerException)
+    {
+        
+    }
     
 }
