0b4f26e957558bfe716466ef69db2ab54575a496
JBrannelid <j.brannelid@icloud.com>
2025-10-17 17:42:05 +0200

    Remove old SendMessage models replaced by DTOs

--- src/FlexNet.Application/Models/SendMessageRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace FlexNet.Application.Models;
-
-public class SendMessageRequest
-{
-        public string Message { get; set; } = string.Empty;
-        public int? Age { get; set; } // Optional for now
-}
