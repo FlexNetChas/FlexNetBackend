e4d6f838db184b04e1787a8f18cf1f22b436ce10
JBrannelid <j.brannelid@icloud.com>
2025-10-20 16:30:18 +0200

    Add UserDescription feature with GET and PATCH support - Add PatchUserDescriptionRequestDto for partial updates - Add IUserDescriptionRepo and IUserDescriptionService interfaces - Implement UserDescriptionService - Implement UserDescriptionRepository for db acess

---
+++ src/FlexNet.Application/Interfaces/IServices/IUserDescriptionService.cs
@@ -0,0 +1,14 @@
+﻿using FlexNet.Application.DTOs.UserDescription.Request;
+using FlexNet.Application.DTOs.UserDescription.Response;
+using FlexNet.Domain.Entities;
+
+namespace FlexNet.Application.Interfaces.IServices;
+
+public interface IUserDescriptionService
+{
+    Task<UserDescriptionResponseDto?> GetUserDescriptionByUserIdAsync(int userId);
+    Task<UserDescriptionResponseDto> PatchUserDescriptionAsync(int userId, PatchUserDescriptionRequestDto request);
+
+    // Simple mapper for UserDescription Entity to UserDescriptionResponseDto
+    UserDescriptionResponseDto MapToDto(UserDescription userDescription);
+}
