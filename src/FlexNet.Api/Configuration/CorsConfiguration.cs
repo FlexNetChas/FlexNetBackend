f58ae58710356f802f24253533fd307e20fd747c
JBrannelid <j.brannelid@icloud.com>
2025-11-07 15:33:07 +0100

    Added Netlify url within allow origins

--- src/FlexNet.Api/Configuration/CorsConfiguration.cs
+++ src/FlexNet.Api/Configuration/CorsConfiguration.cs
@@ -11,7 +11,8 @@
                     policy.WithOrigins(
                           "http://localhost:3000",       // Local development
                           "http://host.docker.internal:3000",   // Docker Desktop
-                          "http://flexnet-frontend:3000"        // Docker Compose internal
+                          "http://flexnet-frontend:3000",       // Docker Compose internal
+                          "https://690dcda9bfa55eda4f317475--flexnetfrontend.netlify.app/" // Netlify Frontend Next.js
                           )
                           .AllowAnyMethod()
                           .AllowAnyHeader()
