18681c83b6104d77578f43be3b4c61ece13bada2
JBrannelid <j.brannelid@icloud.com>
2025-11-11 13:42:17 +0100

    Added a cascade delete behavior to our db configuration ensuring that we remove all information about a user when a user is removed from our system

--- src/FlexNet.Infrastructure/Data/Configuration/AvatarConfiguration.cs
+++ src/FlexNet.Infrastructure/Data/Configuration/AvatarConfiguration.cs
@@ -12,5 +12,12 @@ public class AvatarConfiguration : IEntityTypeConfiguration<Avatar>
         builder.Property(a => a.Personality).HasMaxLength(100).IsRequired();
         builder.Property(a=> a.VoiceEnabled).HasDefaultValue(false);
         builder.Property(a=> a.VoiceSelection).IsRequired().HasMaxLength(50).HasDefaultValue("Default");
+
+        // Cascade delete when a User is deleted from the system/database
+        // This ensures that the associated Avatar is also removed
+        builder.HasOne(a => a.User)
+            .WithOne(u => u.Avatar)
+            .HasForeignKey<Avatar>(a => a.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
