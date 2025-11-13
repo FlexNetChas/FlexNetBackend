bfe2c0828d1eb0cb8ced69f48ea072d9960cd026
JBrannelid <j.brannelid@icloud.com>
2025-11-13 17:33:15 +0100

    Remove uncommen code including unused functions

--- src/FlexNet.Application/Interfaces/IRepositories/IUserRepo.cs
+++ src/FlexNet.Application/Interfaces/IRepositories/IUserRepo.cs
@@ -6,9 +6,6 @@ public interface IUserRepo
 {
     Task<User?> GetByIdAsync(int id);
     Task<User?> GetByEmailAsync(string email);
-    //Task<IEnumerable<User>> GetAllAsync();
     Task<User> AddAsync(User user);
-    //Task<User> UpdateAsync(User user);
     Task<bool> DeleteAsync(int id);
-    //Task<bool> ExistsAsync(int id);
 }
--- src/FlexNet.Application/Interfaces/IServices/IUserService.cs
+++ src/FlexNet.Application/Interfaces/IServices/IUserService.cs
@@ -9,11 +9,8 @@ public interface IUserService
 {
     Task<User?> GetByIdAsync(int id);
     Task<User?> GetByEmailAsync(string email);
-    //Task<IEnumerable<User>> GetAllAsync();
     Task<User> CreateAsync(User user);
-    //Task<User> UpdateAsync(User user);
     Task<bool> DeleteAsync(int id);
-    //Task<bool> ValidatePasswordAsync(string email, string password);
     Task<string> GenerateJwtTokenAsync(User user);
     Task<LoginResponseDto> LoginAsync(LoginRequestDto request);
     Task<RegisterResponseDto> RegisterAsync(RegisterRequestDto request);
